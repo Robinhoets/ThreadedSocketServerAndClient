eafd2cf0841518f765a76de612df7e8c9463e360
Robert Smith <robertets88@gmail.com>
2017-11-08 13:45:38 -0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,60 @@
+/*
+	Robert Smith's Client
+*/
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	sock= socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == -1)
+	{
+		printf("Could not create socket...");
+	}
+	puts("Socket created\n");
+
+	struct sockaddr_in server;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
+
+	if(connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		perror("Connection failed...");
+		return 1;
+	}
+	puts("Connected\n");
+
+	char message[256];
+	char server_reply[256];
+	while(1)
+	{
+		printf("Type message. Press Enter: ");
+		scanf(" %256[^\n]", message);
+
+		// send the message
+		if(send(sock, message, strlen(message), 0) < 0)
+		{
+			printf("Send message failed...");
+			return 1;
+		}
+
+		// Receive a reply
+		if( recv(sock, server_reply, 512, 0) < 0)
+		{
+			printf("recv failed");
+			break;
+		}
+		printf("message (%s) received!\n", message);
+		//puts(server_reply);
+	}
+
+	close(sock);
+
+	return 0;
+}
+
+
---
+++ server.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, int*argv[])
+{	
+
+	
+	int sockid;
+	struct sockaddr_in addport;
+	// create socket id with our (family, type, protocol)
+	// (how we receive and send data, constant strem of characters, default 0)
+	sockid = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockid == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+
+	addport.sin_family = AF_INET;	// AF_INET = IPV4 internet protocl
+	addport.sin_port = htons(8888);	//port number set to 5100 -- htons converts long from host byte order to network byte order
+	addport.sin_addr.s_addr = INADDR_ANY;	// s_addr member of the struct in in_addr contains the host interface address in network byte order
+
+	// BIND	
+	// run error if bind failed
+	if(bind(sockid, (struct sockaddr *) &addport, sizeof(addport)) < 0 )
+	{
+		perror("Error...\nBind failed...\n");
+		return 1;
+	}
+	puts("Bind successful");
+
+	// listen for connection on port 5100
+	listen(sockid, 1);	// on that socket, 1 is the number of participants that can wait for a connection
+	puts("waiting for incoming connecitons...");
+
+	struct sockaddr_in client;	// client is the active participant
+	int socketaddress_size = sizeof(struct sockaddr_in);
+	int client_sock = accept(sockid, (struct sockaddr *) &client, (socklen_t*) &socketaddress_size);
+	if(client_sock < 0)
+	{
+		perror("Accept failed...");
+		return 1;
+	}
+	puts("Accept passed... Connection accepted");
+
+
+	char message_buffer[512];
+	// (sockid, where we store message, message length, flags)
+	int read;	// returns -1 if fail
+
+	while((read = recv(client_sock, message_buffer, 256, 0)) > 0)
+	{
+		write(client_sock, message_buffer, strlen(message_buffer));
+	}
+
+	if(read == 0)
+	{
+		puts("Client disconnected");
+		fflush(stdout);
+	}
+	else if(read == -1)
+	{
+		perror("recv failed");
+	}
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
